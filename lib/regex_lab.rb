2d56df06ddaadb591b8d0894a8f5f0a26d7c0ad0
Tracy Reuther <tracyreuther@gmail.com>
2019-05-18 20:52:47 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,10 @@
 def starts_with_a_vowel?(word)
-match.each do |word|
-  if word.scan(/\A[aeiou]/)
+  if word.match(/\A[aeiouAEIOU]/)
     return true
   else
     return false
   end
   end
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
